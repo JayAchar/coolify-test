32f62b5dabc35546c10f8a3a659c2979b4bef474
J Achar <17378875+JayAchar@users.noreply.github.com>
2024-10-23 17:36:07 +0200

    feat(env): add an environment variable

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,9 @@ ENV PATH="$PNPM_HOME:$PATH"
 RUN corepack enable
 
 FROM base AS build
+WORKDIR /user/src/app
 
-COPY . /usr/src/app
-# Set working directory
-WORKDIR /usr/src/app
+COPY . .
 
 RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
 RUN pnpm run -r build
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,17 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { z } from "zod";
+import { config } from "dotenv";
 
+config();
 const app = new Hono();
+
+const envSchema = z.object({
+  HOST_NAME: z.string(),
+});
+
+type Env = z.infer<typeof envSchema>;
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -13,6 +23,15 @@ app.get("/api/users", (c) => {
     { id: 2, name: "Jane Smith" },
   ];
   return c.json(users);
+});
+
+app.get("/vars", (c) => {
+  const vars = env<Env>(c);
+  const { data, error, success } = envSchema.safeParse(vars);
+  if (!success) {
+    return c.json(error);
+  }
+  return c.json(data.HOST_NAME);
 });
 
 const port = 3000;
